302d79a0ae02678775180c25df2a9450f82a3625
Luke <121516961+Afrozez@users.noreply.github.com>
2023-04-16 16:30:03 -0700

    redesigned the password length so it parses the input value

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 // Assignment Code
-var lengthChoice = window.prompt('(1-30) How many characters would you like the password to be? (1-30)').toUpperCase();
-var userChoice = window.prompt('Would you like to include special characters?').toUpperCase();
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
-function generatePassword() {
-  // This defines the possible characters to use in the password
-  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-  var passwordLength = lengthChoice; // Sets the length of the password to prompt input value
-
-  var password = ""; // This is what the password will be once the for loop is done
-  for (var i = 0; i < passwordLength; i++) {
-    var randomNum = Math.floor(Math.random() * chars.length); // Generate a random number to select a character
-    password += chars.substring(randomNum, randomNum + 1); // Add the selected character to the password
-  }  
-  return password;
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -34,11 +16,22 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  // This defines the possible characters to use in the password
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var passwordLength;
+  do {
+    passwordLength = parseInt(prompt("How long would you like your password to be (8-128)?"));
+  } while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128);
 
-if (userChoice === 'YES') {
-    console.log("yessss");
-} else if (userChoice === 'NO'){
-  console.log('noooooo')
-}else {
-  console.log('invalid entry')
+
+
+
+  var password = ""; // This is what the password will be once the for loop is done
+  
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNum = Math.floor(Math.random() * chars.length); // Generate a random number to select a character
+    password += chars.substring(randomNum, randomNum + 1); // Add the selected character to the password
+  }  
+  return password;
 }
