b72836ce90c2368d56c0610a5712388e85379b58
Luke <121516961+Afrozez@users.noreply.github.com>
2023-04-16 12:04:30 -0700

    added some assignment code with a prompt value

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lengthChoice = window.prompt('(1-30) How many characters would you like the password to be? (1-30)').toUpperCase();
+var userChoice = window.prompt('Would you like to include special characters?').toUpperCase();
+
+console.log(userChoice);
+console.log(lengthChoice);
 
 // Write password to the #password input
 function writePassword() {
